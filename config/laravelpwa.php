bd73c6cd2eca028cb2f86e757d8ead7a624be0d1
jonathan delhoux <jonathan.delhoux@gmail.com>
2020-10-23 15:44:52 +0200

    add icons

--- config/laravelpwa.php
+++ config/laravelpwa.php
@@ -4,7 +4,7 @@ return [
     'name' => 'LaravelPWA',
     'manifest' => [
         'name' => env('APP_NAME', 'My PWA App'),
-        'short_name' => 'PWA',
+        'short_name' => 'MyTDA',
         'start_url' => '/',
         'background_color' => '#ffffff',
         'theme_color' => '#000000',
